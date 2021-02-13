3f692ef0b1b94d47aaaaa68d0119082f2ac014b7
Swapnil Dubey <swapnil.vishu.7027@gmail.com>
2021-02-14 01:55:14 +0530

    form validation added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,7 +17,9 @@ class ContactData extends Component{
                     value:'',
                     validation:{
                         required: true
-                    }
+                    },
+                    valid: false,
+                    touched: false
                 },
                 street:{
                     elementType:'input',
@@ -28,7 +30,9 @@ class ContactData extends Component{
                     value:'',
                     validation:{
                         required: true
-                    }
+                    },
+                    valid: false,
+                    touched: false
                 },
                 zipCode:{
                     elementType:'input',
@@ -38,8 +42,12 @@ class ContactData extends Component{
                     },
                     value:'',
                     validation:{
-                        required: true
-                    }
+                        required: true,
+                        minLength:6,
+                        maxLength: 6
+                    },
+                    valid: false,
+                    touched: false
                 },
                 country:{
                     elementType:'input',
@@ -50,7 +58,9 @@ class ContactData extends Component{
                     value:'',
                     validation:{
                         required: true
-                    }
+                    },
+                    valid: false,
+                    touched: false
                 },   
                 email:{
                     elementType:'input',
@@ -61,7 +71,9 @@ class ContactData extends Component{
                     value:'',
                     validation:{
                         required: true
-                    }
+                    },
+                    valid: false,
+                    touched: false
                 },
                 deliveryMethod:{
                     elementType:'select',
@@ -72,11 +84,11 @@ class ContactData extends Component{
                         ]
                     },
                     value:'',
-                    validation:{
-                        required: true
-                    }
-                },
+                    validation:{},
+                    valid: true
+                }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -104,6 +116,23 @@ class ContactData extends Component{
 
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+        if(!rules){
+            return true;
+        }
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid
+        }
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid
+        }
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -113,8 +142,16 @@ class ContactData extends Component{
         };
 
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        this.setState({orderForm: updatedOrderForm});
+        
+        let formIsValid = true;
+        for(let inputIdentifier in updatedOrderForm){
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+    
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
     render() {
         const formElementArray = [];
@@ -133,9 +170,12 @@ class ContactData extends Component{
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched = {formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
         ))}    
-        <Button btnType="Success" >ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
         </form>
     );
         if(this.state.loading){
